ee1ef4b9883e9e2aebac135a010f9f01e2fa99a4
Zoltan Haindrich <kirk@rxd.hu>
2021-11-04 10:08:33 +0100

    cleanup

--- bingo/shapes.bash
+++ bingo/shapes.bash
@@ -37,9 +37,7 @@ function gen() {
 	done
 
 	args+=" -scale 30% out/s/${num}.${color}.png"
-eval $args
-#	convert -size 100x60 xc:skyblue -fill white -stroke black \
- #         -draw "circle 50,30 40,10"          draw_circle.gif
+	eval $args
 }
 
 for i in `seq 1 6`;do
@@ -47,20 +45,6 @@ for c  in white gray yellow red blue lightgreen;do
 	gen $c $i
 done
 done
-
-if false;then
-for i in `seq 0 5`;do
-for c  in white gray yellow red blue lightgreen;do
-	x=$[ ($i % 3) * a ]
-	y=$[ ($i / 3) * a ]
-
-	convert dice2.jpg -colors 3 -crop ${a}x${a}+$x+$y -trim +repage \
-		-fill $c -draw 'color 10,10 floodfill' -colors 256  \
-		 out/s/$i.$c.jpg
-
-done
-done
-fi
 
 s=30
 
